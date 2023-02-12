cc6c5a2a0804b623b969dfb77a7775463b84aae2
sbarb <sbarb@DESKTOP-GD2PLOU>
2023-02-12 12:59:37 +0100

    Archivos para crear la BBDD

--- Blackjack/src/sql/blackjack.sql
+++ Blackjack/src/sql/blackjack.sql
@@ -5,7 +5,6 @@ drop database if exists blackjack;
 create database
     if not exists blackjack character set latin1 collate latin1_spanish_ci;
 
---show databases;
 
 use blackjack;
 
@@ -15,7 +14,7 @@ create table
         id_jugador int unsigned AUTO_INCREMENT PRIMARY KEY,
         nombre varchar(30) UNIQUE KEY,
         fecha_registro DATE DEFAULT (CURRENT_DATE())
-    );
+    ) engine innodb;
 
 drop table if exists partidas;
 create table
@@ -26,7 +25,7 @@ create table
         puntos_banca int unsigned not null,
         resultado enum ('V','E','D') not null,
         time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
-    );
+    ) engine innodb;
 
 
 alter table partidas 
@@ -36,7 +35,7 @@ Foreign Key (id_jugador) REFERENCES jugadores(id_jugador);
 drop view if exists datos_jugador;
 create view datos_jugador
 as select id_jugador,nombre,fecha_registro, ifnull(sum(resultado='V'),0) victorias, ifnull(sum(resultado='E'),0) empates, 
-    ifnull(sum(resultado='D'),0) derrotas, count (id_partida) partidas_totales
+    ifnull(sum(resultado='D'),0) derrotas, count(id_partida) partidas_totales 
     from jugadores left join partidas using (id_jugador)
     group by id_jugador
-    order by nombre;
+    order by partidas_totales desc;
---
+++ Blackjack/src/sql/tabla_partidas.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO `partidas` (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (1,27,25,17,'D','2023-02-10 21:06:07');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (2,28,25,18,'D','2023-02-10 21:12:03');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (3,28,21,26,'V','2023-02-10 21:12:12');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (4,28,19,25,'V','2023-02-10 21:12:25');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (5,35,20,18,'V','2023-02-11 10:10:44');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (6,35,21,24,'V','2023-02-11 10:10:55');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (7,3,17,17,'E','2023-02-11 10:35:21');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (8,3,18,19,'D','2023-02-11 10:35:44');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (9,36,21,17,'V','2023-02-11 10:38:58');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (10,3,22,22,'E','2023-02-11 10:39:23');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (11,3,20,22,'V','2023-02-11 10:39:36');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (12,3,17,19,'D','2023-02-11 10:39:47');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (13,5,19,17,'V','2023-02-11 10:41:55');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (14,42,21,20,'V','2023-02-11 12:40:19');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (15,9,19,21,'D','2023-02-11 12:50:38');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (16,3,18,19,'D','2023-02-11 13:00:54');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (17,3,21,18,'V','2023-02-11 13:01:09');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (18,3,21,17,'V','2023-02-11 19:32:45');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (19,3,21,20,'V','2023-02-11 19:38:07');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (20,3,22,21,'D','2023-02-11 19:38:21');
+INSERT INTO `partidas`  (`id_partida`,`id_jugador`,`puntos_jugador`,`puntos_banca`,`resultado`,`time_stamp`) VALUES (21,43,18,17,'V','2023-02-11 19:51:53');
