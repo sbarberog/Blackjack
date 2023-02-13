5ba0b206a4748dc3cf3f9cc42d77966739c00018
sbarb <sbarb@DESKTOP-GD2PLOU>
2023-02-13 22:48:41 +0100

    Actualizada BBDD H2

--- Blackjack/src/sql/blackjack_h2.sql
+++ Blackjack/src/sql/blackjack_h2.sql
@@ -1,5 +1,10 @@
-
+drop view if exists datos_jugador;
+drop table if exists partidas;
 drop table if exists jugadores;
+
+set collation spanish strength identical;
+
+
 create table
     if not exists jugadores (
         id_jugador int AUTO_INCREMENT PRIMARY KEY,
@@ -7,7 +12,6 @@ create table
         fecha_registro DATE DEFAULT (CURRENT_DATE())
     ) ;
 
-drop table if exists partidas;
 create table
     if not exists partidas (
         id_partida int AUTO_INCREMENT PRIMARY KEY,
@@ -23,10 +27,10 @@ alter table partidas
 ADD CONSTRAINT fk_partidas_id_jugador_jugadores
 Foreign Key (id_jugador) REFERENCES jugadores(id_jugador);
 
-drop view if exists datos_jugador;
+
 create view datos_jugador
 as select j.id_jugador,nombre,fecha_registro, ifnull(sum(resultado='V'),0) victorias, ifnull(sum(resultado='E'),0) empates, 
     ifnull(sum(resultado='D'),0) derrotas, count(id_partida) partidas_totales 
     from jugadores j left join partidas p using (id_jugador)
-    group by id_jugador
+    group by j.id_jugador
     order by partidas_totales desc;
