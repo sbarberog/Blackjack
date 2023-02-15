c64257f9da3d40257cdbc889ebc308f0216e13e5
sbarb <sbarb@DESKTOP-GD2PLOU>
2023-02-15 20:37:39 +0100

    creación de BBDD

--- Blackjack/src/sql/blackjack.sql
+++ Blackjack/src/sql/blackjack.sql
@@ -13,7 +13,8 @@ create table
     if not exists jugadores (
         id_jugador int  AUTO_INCREMENT PRIMARY KEY,
         nombre varchar(30) UNIQUE,
-        fecha_registro DATE DEFAULT (CURRENT_DATE())
+        fecha_registro DATE DEFAULT (CURRENT_DATE()),
+        fichas int default 100
     ) engine innodb;
 
 drop table if exists partidas;
@@ -24,7 +25,9 @@ create table
         puntos_jugador int  not null,
         puntos_banca int  not null,
         resultado enum ('V','E','D') not null,
-        time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+        time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+        apuesta INT,
+        blackjack boolean default false
     ) engine innodb;
 
 
@@ -34,8 +37,8 @@ Foreign Key (id_jugador) REFERENCES jugadores(id_jugador);
 
 drop view if exists datos_jugador;
 create view datos_jugador
-as select j.id_jugador,nombre,fecha_registro, ifnull(sum(resultado='V'),0) victorias, ifnull(sum(resultado='E'),0) empates, 
+as select j.id_jugador, nombre, fecha_registro, fichas, ifnull(sum(resultado='V'),0) victorias, ifnull(sum(resultado='E'),0) empates, 
     ifnull(sum(resultado='D'),0) derrotas, count(id_partida) partidas_totales 
     from jugadores j left join partidas p using (id_jugador)
     group by id_jugador
-    order by partidas_totales desc;
+    order by fichas desc;
